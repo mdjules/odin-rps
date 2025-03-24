339cabd5dc43a16b04e1a81e65482066399bfe11
Michael Juliano <mdj.michaeljuliano@gmail.com>
2025-03-23 22:03:40 -0400

    Added functionality to update score and check who wins
    updateDisplayedScore() function was created to
    update score shown on html. The function was Added
    to the playRound() function at the end to run each
    time a player clicks a button.
    
    checkWinner() function uses an if statement to
    check when either the player or the computer reach
    a score of 5 first. Whichever gets to 5 first is
    displayed a message that indicates that they have
    won.

--- rock-paper-scissor.js
+++ rock-paper-scissor.js
@@ -31,6 +31,7 @@ function getComputerChoice () {
 };*/
 
 function playRound (humanChoice, computerChoice) {
+    
     if (humanChoice == "Rock" && computerChoice == "Scissors") {
         console.log("You win! Rock beats scissors!");
         humanScore = ++humanScore;
@@ -38,6 +39,8 @@ function playRound (humanChoice, computerChoice) {
     else if (humanChoice == "Paper" && computerChoice == "Rock") {
         console.log("You win! Paper beats rock!");
         humanScore = ++humanScore;
+       
+        
     }
     else if (humanChoice == "Scissors" && computerChoice == "Paper") {
         console.log("You win! Scissors beats paper!");
@@ -48,30 +51,12 @@ function playRound (humanChoice, computerChoice) {
     }
     else {
         console.log("You lose! Computer chose " + computerChoice + ". " + computerChoice + " beats " + humanChoice);
-        computerScore = ++computerScore
-    }
-};
-
-//counter = 0
-
-function playGame () {
-    //while (counter < 6) {
-    //let humanSelection = getHumanChoice();
-    let computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-    console.log("Human score is: " + humanScore);
-    console.log("Computer score is: " + computerScore);
-    //++counter;
-    //}
-
-    if (humanScore > computerScore) {
-        console.log("Congrats you beat the computer!");
-    }
-        else {
-            console.log("The computer beat you...better luck next time.");
-        };
-    ;
-};
+        computerScore = ++computerScore;
+    };
+        updateDisplayedScore();
+        checkForWinner();
+    
+    };
 
 //rock button event listener uses playRound() as callback function
 
@@ -97,6 +82,46 @@ scissorsBtn.addEventListener("click", function() {
     playRound("Scissors", getComputerChoice())
 });
 
-//playGame();
+const scoreTable = document.querySelector("#scoreTable");
+
+scoreTable.textContent = "Score Table:";
+
+//display human score on html
+let humanDisplayedScore = document.querySelector("#humanDisplayedScore");
+
+humanDisplayedScore.textContent = `Player Score: ${humanScore}`;
+
+//display computerScore score on html
+let computerDisplayedScore = document.querySelector("#computerDisplayedScore")
+
+computerDisplayedScore.textContent = `Computer Score: ${computerScore}`;
+
+function updateDisplayedScore() {
+    let humanDisplayedScore = document.querySelector("#humanDisplayedScore");
+    humanDisplayedScore.textContent = `Player Score: ${humanScore}`;
+
+    let computerDisplayedScore = document.querySelector("#computerDisplayedScore");
+    computerDisplayedScore.textContent = `Computer Score: ${computerScore}`;
+}
+
+//intro message
+let winner = document.querySelector("#winner");
+winner.textContent = "Welcome to Rock, Paper, Scissors! See if you can beat the computer!"
+
+function checkForWinner() {
+
+    if (humanScore == 5) {
+        let winner = document.querySelector("#winner");
+        winner.textContent = "You beat the computer!!!";
+    }
+    else if (computerScore == 5) {
+        let winner = document.querySelector("#winner");
+        winner.textContent = "The computer beat you!"
+    }
+    else {
+        let winner = document.querySelector("#winner");
+        winner.textContent = "Make a choice...First one to 5 wins!!";
+    }
+}
 
 //playRound();
